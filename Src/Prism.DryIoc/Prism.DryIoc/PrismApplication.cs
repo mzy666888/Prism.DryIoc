60a3fe7317d3efb62b0b612e657273ca308b444c
mzy666888 <mzy_666888@126.com>
2023-02-09 08:47:46 +0800

    update

---
+++ Src/Prism.DryIoc/Prism.DryIoc/PrismApplication.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using DryIoc;
+using Prism.Ioc;
+
+namespace Prism.DryIoc
+{
+    public abstract class PrismApplication:PrismApplicationBase
+    {
+        protected virtual Rules CreateContainerRules() => DryIocContainerExtension.DefaultRules;
+        protected override IContainerExtension CreateContainerExtension()
+        {
+            return new DryIocContainerExtension(new Container(CreateContainerRules()));
+        }
+
+        protected override void RegisterFrameworkExceptionTypes()
+        {
+            ExceptionExtensions.RegisterFrameworkExceptionType(typeof(ContainerException));
+        }
+    }
+}
